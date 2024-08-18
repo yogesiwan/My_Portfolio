68278b758ff2f3af24b6bf79c140c1e4f3c0fc8c
yogesh <yogesh@yogeshs-MacBook-Air.local>
2024-08-18 12:37:57 +0530

    first commit

---
+++ src/data/data.js
@@ -0,0 +1,25 @@
+import { AiFillAppstore } from "react-icons/ai";
+import { FaMobile, FaGlobe } from "react-icons/fa";
+import { SiProgress } from "react-icons/si";
+
+// Features Data
+export const featuresData = [
+  {
+    id: 1,
+    icon: <AiFillAppstore />,
+    title: "App Development",
+    des: "Building responsive and interactive web applications using modern front-end technologies.",
+  },
+  {
+    id: 2,
+    icon: <SiProgress />,
+    title: "SEO Optimization",
+    des: "Enhancing website performance and search engine visibility through best coding practices.",
+  },
+  {
+    id: 3,
+    icon: <FaMobile />,
+    title: "Mobile-Friendly Design",
+    des: "Ensuring websites are fully responsive and optimized for all devices.",
+  },
+];
